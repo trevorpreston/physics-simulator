d4f0ef4bb63986c5e4d7296d2bae986101f3a59f
Trevor Preston <trevorpreston4@gmail.com>
2017-02-16 13:05:35 -0500

    add displacement calculation function

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log('javascript connected')
+
+const accel = 9.8
+
+let initialVelocity = 0
+let finalVelocity
+
+// v = a*t
+
+
+function updateVelocity(timeSeconds) {
+	finalVelocity = initialVelocity + accel*timeSeconds
+	console.log(finalVelocity)
+}
+
+
+//calculates displacement with given time, acceleration, initial v
+function dropBall(time) {
+	let displacement //in meters
+	displacement = initialVelocity*time + .5*accel*(time*time)
+	console.log(displacement)
+}
+
+dropBall(10)
